3fdb3194167a72b643281921ab88a5bc46768278
Allen Zhao <allen.zhao@gmail.com>
2025-08-10 19:46:32 -0400

    Fix static folder issue

--- strava_dash/settings.py
+++ strava_dash/settings.py
@@ -142,7 +142,7 @@ LOCALE_PATHS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'strava_dash/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'strava_web/static'),
 ]
